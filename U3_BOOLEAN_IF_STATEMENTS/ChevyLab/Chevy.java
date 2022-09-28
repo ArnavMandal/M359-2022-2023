a3b86281cf90727c42cd04faf3590fad7d897d57
ArnavMandal <113718112+ArnavMandal@users.noreply.github.com>
2022-09-28 13:49:07 -0500

    Chevy

--- U3_BOOLEAN_IF_STATEMENTS/ChevyLab/Chevy.java
+++ U3_BOOLEAN_IF_STATEMENTS/ChevyLab/Chevy.java
@@ -3,45 +3,67 @@ package U3_BOOLEAN_IF_STATEMENTS.ChevyLab;
 public class Chevy {
     private int year;
     private int miles;
-    private double fuelEfficiency;
+    private double mpg;
     private double price;
     private double priceWithUpgrades;
     private double grandTotal;
     private String model;
     private String color;
-    private boolean hasLuxPackage;
+    private boolean hasLuxuryPkg;
     private boolean has4WDPkg;
-    private boolean hasSportsPackage;
+    private boolean hasSportsPkg;
+
     final double TAX_RATE = .122;
 
     // Constructors
     public Chevy(){
         this.year = 2021;
         this.miles = 0;
-        this.fuelEfficiency = 35;
+        this.mpg = 35;
         this.price = 16000;
         this.model = "Trax";
         this.color = "White";
-        this.hasLuxPackage = false;
+        this.hasLuxuryPkg = false;
         this.has4WDPkg = false;
-        this.hasSportsPackage = false;
+        this.hasSportsPkg = false;
 
 
 
     }
 
 
-    public Chevy(int year, int miles, double fuelEfficiency, double price, String model, String color, boolean hasLuxPackage, boolean has4WDPkg, boolean hasSportsPackage) {
+    public Chevy(int year, int miles, double mpg, double price, String model, String color, boolean hasLuxuryPkg, boolean has4WDPkg, boolean hasSportsPackage) {
         this.year = year;
         this.miles = miles;
-        this.fuelEfficiency = fuelEfficiency;
+        this.mpg = mpg;
         this.price = price;
         this.model = model;
         this.color = color;
-        this.hasLuxPackage = hasLuxPackage;
+        this.hasLuxuryPkg = hasLuxuryPkg;
         this.has4WDPkg = has4WDPkg;
-        this.hasSportsPackage = hasSportsPackage;
+        this.hasSportsPkg = hasSportsPackage;
     }
+
+    public boolean equals(Chevy other){
+        if(this.model.equals(other.model) && this.color.equals(other.color) && this.miles<200 && other.miles<200){
+        return true;
+        }else if(this.model.equals(other.model) && this.color.equals(other.color) && this.miles>=200 && other.miles>=200){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public int compareTo(Chevy other){
+        if(this.miles > other.miles){
+            return 1;
+        }else if(this.miles == other.miles){
+            return 0;
+        }else{
+            return -1;
+        }
+    }
+
 
     public int getYear() {
         return year;
@@ -59,19 +81,19 @@ public class Chevy {
         this.miles = miles;
     }
 
-    public double getFuelEfficiency() {
-        return fuelEfficiency;
+    public double getMpg() {
+        return mpg;
     }
 
     public void setFuelEfficiency(double fuelEfficiency) {
-        this.fuelEfficiency = fuelEfficiency;
+        this.mpg = mpg;
     }
 
-    public double getPrice() {
+    public double getBasePrice() {
         return price;
     }
 
-    public void setPrice(double price) {
+    public void setBasePrice(double price) {
         this.price = price;
     }
 
@@ -107,27 +129,27 @@ public class Chevy {
         this.color = color;
     }
 
-    public boolean isHasLuxPackage() {
-        return hasLuxPackage;
+    public boolean getLuxuryPkg() {
+        return hasLuxuryPkg;
     }
 
-    public void setHasLuxPackage(boolean hasLuxPackage) {
-        this.hasLuxPackage = hasLuxPackage;
+    public void setLuxuryPkg(boolean hasLuxuryPkg) {
+        this.hasLuxuryPkg = hasLuxuryPkg;
     }
 
-    public boolean getHas4WDPkg() {
+    public boolean get4WDPkg() {
         return has4WDPkg;
     }
 
-    public void setHas4WDPkg(boolean has4wdPackage) {
+    public void set4WDPkg(boolean has4wdPackage) {
         this.has4WDPkg = has4wdPackage;
     }
 
-    public boolean isHasSportsPackage() {
-        return hasSportsPackage;
+    public boolean getSportsPkg() {
+        return hasSportsPkg;
     }
 
-    public void setHasSportsPackage(boolean hasSportsPackage) {
-        this.hasSportsPackage = hasSportsPackage;
+    public void setSportsPackage(boolean hasSportsPkg) {
+        this.hasSportsPkg = hasSportsPkg;
     }
 }
