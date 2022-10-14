02095dcee39f496859016697ba52e6d46b065ff1
ArnavMandal <113718112+ArnavMandal@users.noreply.github.com>
2022-10-14 13:49:34 -0500

    NEW1

---
+++ U4_LOOPS/Notes4_5_CodeAnalysis.java
@@ -0,0 +1,43 @@
+package U4_LOOPS;
+
+public class Notes4_5_CodeAnalysis {
+    public static void main(String[] args) {
+       // #1
+       outputBreak("Challenge 1: Number Pyramid 1 to 5");
+       for (int i = 6; i > 1; i--){
+           for (int k = 1; k < i; k++){
+               System.out.print(k + " ");
+           }
+           System.out.println();
+       }
+       //#2
+        outputBreak("Challenge 2: Number Pyramid 5 to 1");
+        for (int i = 5; i > 0; i--){
+            for (int k = 0; k < i; k++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+       //#3
+        outputBreak("Challenge 3:");
+        everyOtherLetterPyramid("FremdVikings");
+       //#4
+
+
+    }
+
+    public static void outputBreak(String title){
+        System.out.println();
+        System.out.println();
+        System.out.println(title);
+        System.out.println();
+    }
+    public static void everyOtherLetterPyramid(String str){
+        String out =  "";
+        for (int i = 0; i < str.length() ; i += 2){
+            out += str.charAt(i);
+            System.out.println(out);
+        }
+
+    }
+}
