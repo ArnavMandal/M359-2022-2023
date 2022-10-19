af5abc22702ff4ece0d925672544f79375f69880
ArnavMandal <113718112+ArnavMandal@users.noreply.github.com>
2022-10-19 13:49:41 -0500

    PigLatin day 1

--- U4_LOOPS/PigLatinLab/PigLatin.java
+++ U4_LOOPS/PigLatinLab/PigLatin.java
@@ -1,38 +1,43 @@
 package U4_LOOPS.PigLatinLab;
 
-import java.util.Locale;
+
 
 public class PigLatin {
 
-
     public static String translateWordToPigLatin(String str){
-        // In case there is a vowel at the start, add "yay" to the end
-        // and return the String
-        if(str.substring(0, 1).equalsIgnoreCase("a") ||
-           str.substring(0, 1).equalsIgnoreCase("e") ||
-           str.substring(0, 1).equalsIgnoreCase("i") ||
-           str.substring(0, 1).equalsIgnoreCase("o") ||
-           str.substring(0, 1).equalsIgnoreCase("u")){
-
-            str+="yay";
+        if(vowelHelper(str)){
+            str += "yay";
+            str.toLowerCase();
             return str;
         }else{
-
-            String consonant = "";
-
-            for(int i = 0; i < str.length(); i++){
-                if(str.substring(i, i+1).equalsIgnoreCase("a") ||
-                        str.substring(i, i+1).equalsIgnoreCase("e") ||
-                        str.substring(i, i+1).equalsIgnoreCase("i") ||
-                        str.substring(i, i+1).equalsIgnoreCase("o") ||
-                        str.substring(i, i+1).equalsIgnoreCase("u")){
-                    consonant = str.substring(0, i);
-                    str = str.substring(i) + consonant + "ay";
-                    return str;
-                }else{
-                return str;
+            for (int i = 0; i < str.length(); i++){
+                if(!vowelHelper(str)){
+                    str = str.substring(1) + str.substring(0, 1);
                 }
             }
+            str += "ay";
+            str.toLowerCase();
+            return str;
         }
+}
+/*
+public static String toPigLatin(String str){
+        String sentence = str.toLowerCase();
+        String temp = "";
+
+}
+*/
+
+public static boolean vowelHelper(String word){
+    if     (word.substring(0, 1).equalsIgnoreCase("a") ||
+            word.substring(0, 1).equalsIgnoreCase("e") ||
+            word.substring(0, 1).equalsIgnoreCase("i") ||
+            word.substring(0, 1).equalsIgnoreCase("o") ||
+            word.substring(0, 1).equalsIgnoreCase("u")){
+            return true;
+
+    }else{
+            return false;
+    }
     }
 }
