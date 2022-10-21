b60bb8c1306130345856e262f9707c58bc5f0cc7
ArnavMandal <113718112+ArnavMandal@users.noreply.github.com>
2022-10-21 13:49:08 -0500

    PigLatin Comments

--- U4_LOOPS/PigLatinLab/PigLatin.java
+++ U4_LOOPS/PigLatinLab/PigLatin.java
@@ -4,36 +4,71 @@ package U4_LOOPS.PigLatinLab;
 
 public class PigLatin {
     /**
-     * This function takes a phrase with multiple words and returns a String with the phrase in pigLatin
-     * @param str
-     * @return
+     * This method takes a phrase with multiple words and returns a String with the phrase in pigLatin,
+     * utilizing the translateWordToPigLatin method
+     * @param str this is the sentence that you want translated into pig latin
+     * @return returns the translated sentence
      */
     public static String toPigLatin(String str){
-
+        /* We start with adding a space to the end of intput string,
+        // and initializes a newSentence String which will be outputted
+        at the end
+        */
         str += " ";
         String newSentence = "";
 
+        /*
+           this loop first separates the String str into words, by chunking them
+           based on the location of a space or " " and translates them using
+           the translateWordToPigLatin method, adding that to the newSentence string.
+           We then chop off the str after that space " " and repeat until there are no
+           words lefts.
+         */
+
         while(str.length() != 0){
+
             newSentence += translateWordToPigLatin(str.substring(0, str.indexOf(" ")));
+
             newSentence += " ";
+
             str = str.substring(str.indexOf(" ") + 1);
         }
+
+
+        // makes the first letter of the string uppercase and returns it
+
         newSentence = newSentence.substring(0, 1).toUpperCase() + newSentence.substring(1, newSentence.length() - 1);
+
         return newSentence;
     }
+
+    /**
+     *  this method translates a single string to pig latin and returns the translated string
+     * @param str the word you want to translate
+     * @return the translated word
+     */
     public static String translateWordToPigLatin(String str){
+        // this if statement checks if the input string starts with a vowel
+        // if it does, we then add "yay" to the end and return the string
         if(vowelHelper(str)){
             str += "yay";
             return str.toLowerCase();
-        }else{
+        }
+
+        // if the string doesnt start with a vowel the method loops
+        else{
             for (int i = 0; i < str.length(); i++){
                 if(!vowelHelper(str)){
+
                     str = str.substring(1) + str.substring(0, 1);
                 }
             }
             str += "ay";
+
             return str.toLowerCase();
         }
+
+
 }
 
 public static boolean vowelHelper(String word){
@@ -42,10 +77,14 @@ public static boolean vowelHelper(String word){
             word.substring(0, 1).equalsIgnoreCase("i") ||
             word.substring(0, 1).equalsIgnoreCase("o") ||
             word.substring(0, 1).equalsIgnoreCase("u")){
-            return true;
+
+
+        return true;
 
     }else{
-            return false;
+
+
+        return false;
     }
     }
 }
