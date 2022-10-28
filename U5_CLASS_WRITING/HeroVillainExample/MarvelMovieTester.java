62886de2e586b0924520394f5345296974861cff
ArnavMandal <113718112+ArnavMandal@users.noreply.github.com>
2022-10-28 13:49:37 -0500

    HERO V VILLAIN

--- U5_CLASS_WRITING/HeroVillainExample/MarvelMovieTester.java
+++ U5_CLASS_WRITING/HeroVillainExample/MarvelMovieTester.java
@@ -17,5 +17,44 @@ public class MarvelMovieTester {
         System.out.println(SuperMan);
         System.out.println();
         System.out.println(reverseFlash);
+
+
+        // Have these two battle
+    }
+
+    /**
+     * Simulates a battle between hero & villain. Whoever has greater power strength wins.
+     * if power level is equal, we flip a coin to see who wins
+     * After the battle...
+     *      1. Winner gains 10% of loser's power level
+     *      2. Lose will lose between 5% - 35% of their own power strength
+     * @param a
+     * @param b
+     */
+    public static void battle(SuperHero a, SuperVillian b){
+        double aLvl = a.getSuperPower().getPowerStrength();
+        double bLvl = a.getSuperPower().getPowerStrength();
+    if (aLvl > bLvl){
+        System.out.println("Hero won!!");
+
+        aLvl += .1 * bLvl;
+
+        a.getSuperPower().setPowerStrength(aLvl);
+
+        int lossPrct  = (int)(Math.random()*(35-5+1)) + 5;
+        double newVillian = bLvl - (lossPrct/100.0)*bLvl;
+        b.getSuperPower().setPowerStrength(newVillian);
+    } else if (bLvl > aLvl){
+        System.out.println("Villain won!!");
+        bLvl += .1 * aLvl;
+
+        b.getSuperPower().setPowerStrength(bLvl);
+
+        int lossPrct  = (int)(Math.random()*(35-5+1)) + 5;
+        double newHero = aLvl - (lossPrct/100.0)*aLvl;
+        a.getSuperPower().setPowerStrength(newHero);
+    }else{
+        System.out.println("lolol");
+    }
     }
 }
--- U5_CLASS_WRITING/HeroVillainExample/Power.java
+++ U5_CLASS_WRITING/HeroVillainExample/Power.java
@@ -3,16 +3,23 @@ package U5_CLASS_WRITING.HeroVillainExample;
 public class Power {
     private String power;
 
-    public int powerStrength;
+    public double powerStrength;
 
     public String toString(){
         String output = "";
+
 
         output += "Name of the superpower: " + this.power + "\n";
         output += "Power Strength: " + this.powerStrength;
         return output;
     }
-    public Power(String power, int powerStrength) {
+
+    /**
+     * full constructor of power object
+     * @param power name of superpower
+     * @param powerStrength this is the strength of your power (1-10)
+     */
+    public Power(String power, double powerStrength) {
         this.power = power;
         this.powerStrength = powerStrength;
     }
@@ -25,11 +32,11 @@ public class Power {
         this.power = power;
     }
 
-    public int getPowerStrength() {
+    public double getPowerStrength() {
         return powerStrength;
     }
 
-    public void setPowerStrength(int powerStrength) {
+    public void setPowerStrength(double powerStrength) {
         this.powerStrength = powerStrength;
     }
 }
