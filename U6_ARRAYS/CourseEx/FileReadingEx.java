fcee619554314a20698422c1cd8495bbe096ac4b
ArnavMandal <113718112+ArnavMandal@users.noreply.github.com>
2022-12-02 13:48:15 -0600

    435

--- U6_ARRAYS/CourseEx/FileReadingEx.java
+++ U6_ARRAYS/CourseEx/FileReadingEx.java
@@ -5,26 +5,49 @@ import java.util.Scanner;
 
 public class FileReadingEx {
     public static void main(String[] args) throws FileNotFoundException {
-        File myFile = new File("oneSchedule.txt");
+        File myFile = new File("studentScheduleData.txt");
         Scanner fileIn = new Scanner(myFile);
-
+        // first line should be the # of students
+        int numStudents = fileIn.nextInt();
+        fileIn.nextLine(); // dummy read
+        // create an array of student objs
+        Student[] myStudents = new Student[numStudents];
         // First, read in student's name
 
-        String stName = fileIn.nextLine();
-        Course[] sched = new Course[8];
-        for (int i = 0; i < 8; i++) {
-            String teacherName = fileIn.nextLine();
-            String className = fileIn.nextLine();
-            String letterGrade = fileIn.nextLine();
-            int periodNum = fileIn.nextInt();
-            if (fileIn.hasNextLine()){
-                fileIn.nextLine();                   //dummy read to clear newLine after #
-            }
+        for (int j = 0; j < numStudents; j++) {
 
-            Course myCourse = new Course(teacherName, className, letterGrade, periodNum);
-            sched[i] = myCourse;
+            String stName = fileIn.nextLine();
+
+            Course[] sched = new Course[8];
+
+            for (int i = 0; i < 8; i++) {
+
+                String teacherName = fileIn.nextLine();
+
+                String className = fileIn.nextLine();
+
+                String letterGrade = fileIn.nextLine();
+
+                int periodNum = fileIn.nextInt();
+
+                if (fileIn.hasNextLine()) {
+
+                    fileIn.nextLine();                   //dummy read to clear newLine after #
+                }
+
+                Course myCourse = new Course(teacherName, className, letterGrade, periodNum);
+
+                sched[i] = myCourse;
+            }
+            // now we know the students name and course, we can create the obj
+
+            Student curStudent = new Student(stName, sched);
+
+            myStudents[j] = curStudent;
         }
-        for (Course c: sched){
+
+        for (Student c: myStudents){
+
             System.out.println(c);
         }
     }
