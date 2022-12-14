3d4c099b68e2ff13693975ef92a3ce44a8e52fd5
ArnavMandal <113718112+ArnavMandal@users.noreply.github.com>
2022-12-14 13:47:51 -0600

    9087

--- U6_ARRAYS/Lab/TriviaGame.java
+++ U6_ARRAYS/Lab/TriviaGame.java
@@ -1,6 +1,7 @@
 package U6_ARRAYS.Lab;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TriviaGame {
@@ -9,12 +10,15 @@ public class TriviaGame {
     File myFile = new File("trivia.txt");
     Scanner fileIn = new Scanner(myFile);
 
-    public void initQuestion(){
+    public void initQuestion() throws FileNotFoundException {
         int numQuestions = fileIn.nextInt();
         fileIn.nextLine();
         questions = Question[numQuestions];
         for (int i = 0; i < questions.length; i++){
-            questions[i] = new Question()
+            questions[i] = new Question(questions[i].getQuestion(), questions[i].getAnswer(),
+                                        questions[i].getChoices(), questions[i].getValue());
         }
+
+
     }
 }
