aa64c6b827c8d66cbd27fbc96b5df7cbc8bd3fb7
Arnav <arnavmandal06@gmail.com>
2022-12-16 01:01:05 -0600

    79

--- U6_ARRAYS/Lab/TriviaDriver.java
+++ U6_ARRAYS/Lab/TriviaDriver.java
@@ -1,7 +1,92 @@
 package U6_ARRAYS.Lab;
 
-public class TriviaDriver {
-    public static void main(String[] args) {
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class TriviaDriver {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        TriviaGame testGame = new TriviaGame();
+        welcome();
+
+        Question[] totalQuestions = testGame.getQuestions();
+
+        for (int i = 0; i < totalQuestions.length; i++){
+            //get random number
+            int rand = (int)(Math.random()* totalQuestions.length);
+
+            if(i > 0){
+                // repeats random number
+                while (totalQuestions[rand] == totalQuestions[i]){
+                rand = (int)(Math.random()* totalQuestions.length);
+                }
+            }
+        }
+    }
+
+
+
+
+    public static void separateText(){
+        for (int i =0; i < 53; i++){
+            System.out.print("*");
+        }
+    }
+
+    public static boolean playAgain(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Would you like to play again? (yes/no)");
+
+        String response = in.nextLine();
+
+        if(response.equalsIgnoreCase("yes")){
+            System.out.print("Let the game go on!");
+            return true;
+        }
+        return false;
+    }
+
+    public static void closingStatement(){
+        System.out.println();
+        System.out.println("Thanks for playing food trivia, hope you had fun!");
+    }
+    /**
+     * this static method is meant to welcome the player
+     * asks the name of player and if they want to start
+     * the game, and reads out general instructions
+     */
+    public static void welcome(){
+        separateText();
+        //welcome statement
+        System.out.println("");
+        System.out.println("\n" + " Welcome to Food Trivia! ");
+        separateText();
+        //asking for username
+        Scanner userName = new Scanner(System.in);
+
+        System.out.print("\nWhat's your Username? ");
+
+        String name = userName.nextLine();
+        //greets player by name
+        System.out.println("\n" + "Greetings " + name + ", Welcome to Food Trivia");
+        //instructions
+        System.out.println("\nInstructions: There are 15 Questions that you will need to answer.\n" +
+                            "There are 4 choices but only 1 is correct. Your score is determined by" +
+                            "the number of correct answers");
+        Scanner userStart = new Scanner(System.in);
+        //make user type start
+        System.out.println("\nType /start/ to enter game.");
+
+        String start = userStart.nextLine();
+
+        while(!start.equalsIgnoreCase("start")){
+            System.out.println("Error. Please type /Start/");
+            userStart = new Scanner(System.in);
+            start = userStart.nextLine();
+        }
+
+        separateText();
+
+        System.out.println("");
     }
 }
--- U6_ARRAYS/Lab/TriviaGame.java
+++ U6_ARRAYS/Lab/TriviaGame.java
@@ -5,20 +5,92 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TriviaGame {
-    Question[] questions;
-    int streak;
-    File myFile = new File("trivia.txt");
-    Scanner fileIn = new Scanner(myFile);
+   private Question[] questions;
 
-    public void initQuestion() throws FileNotFoundException {
+   private int streak;
+
+   private int correctNum;
+
+    private static int score;
+
+    public TriviaGame() throws FileNotFoundException {
+
+    this.score = 0;
+
+    this.correctNum = 0;
+
+    this.streak = 0;
+
+    this.questions = initQuestion();
+    }
+    public Question[] initQuestion() throws FileNotFoundException{
+
+        File myFile = new File("trivia.txt");
+        Scanner fileIn = new Scanner(myFile);
+
         int numQuestions = fileIn.nextInt();
+
         fileIn.nextLine();
-        questions = Question[numQuestions];
-        for (int i = 0; i < questions.length; i++){
-            questions[i] = new Question(questions[i].getQuestion(), questions[i].getAnswer(),
-                                        questions[i].getChoices(), questions[i].getValue());
+
+        Question[] list = new Question[numQuestions];
+
+        for (int i = 0; i < list.length; i++){
+           String name = fileIn.nextLine();
+
+           String a = fileIn.nextLine();
+
+           String b = fileIn.nextLine();
+
+           String c = fileIn.nextLine();
+
+           String d = fileIn.nextLine();
+
+           String correctChoice = fileIn.nextLine();
+
+           int questionVal = fileIn.nextInt();
+
+           if(fileIn.hasNextLine())
+               fileIn.nextLine();
+
+           String[] allChoices = {a, b, c, d};
+
+           Question newQuestion = new Question(name, correctChoice, allChoices, questionVal);
+
+           list[i] = newQuestion;
         }
+        return list;
 
+    }
 
+    public int getStreak() {
+        return streak;
+    }
+
+    public void setStreak(int streak) {
+        this.streak = streak;
+    }
+
+    public int getCorrectNum() {
+        return correctNum;
+    }
+
+    public void setCorrectNum(int correctNum) {
+        this.correctNum = correctNum;
+    }
+
+    public static int getScore() {
+        return score;
+    }
+
+    public static void setScore(int score) {
+        TriviaGame.score = score;
+    }
+
+    public Question[] getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(Question[] questions) {
+        this.questions = questions;
     }
 }
