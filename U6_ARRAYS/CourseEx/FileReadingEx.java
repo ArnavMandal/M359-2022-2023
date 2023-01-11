cfaefed60ebb63636c8d990c01b80973e2056284
ArnavMandal <113718112+ArnavMandal@users.noreply.github.com>
2023-01-11 13:22:27 -0600

    546

--- U6_ARRAYS/CourseEx/FileReadingEx.java
+++ U6_ARRAYS/CourseEx/FileReadingEx.java
@@ -50,5 +50,6 @@ public class FileReadingEx {
 
             System.out.println(c);
         }
+        // hello cat
     }
 }
---
+++ U7_ARRAYLISTS/Examples/Notes_7_3.java
@@ -0,0 +1,7 @@
+package U7_ARRAYLISTS.Examples;
+
+public class Notes_7_3 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
